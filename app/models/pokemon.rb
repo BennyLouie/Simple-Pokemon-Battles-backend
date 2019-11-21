d60836ce5a70c342eaae9e050e34abb861c95a97
BennyLouie <bennylouie1412@gmail.com>
2019-11-21 13:25:15 -0500

    added model relations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,4 @@
 class Pokemon < ApplicationRecord
+    has_many :captures
+    has_many :users, through: :captures
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :captures
+    has_many :pokemons, through: :captures
 end
