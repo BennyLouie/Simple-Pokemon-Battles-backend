2a441b2dda53a707ee2d93d960c81f8f12519857
BennyLouie <bennylouie1412@gmail.com>
2019-11-21 11:40:19 -0500

    created pokemon migration file

---
+++ db/migrate/20191121163937_create_pokemons.rb
@@ -0,0 +1,18 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.string :front_img
+      t.string :back_img
+      t.integer :lv
+      t.integer :exp
+      t.integer :hp
+      t.integer :atk
+      t.integer :def
+      t.integer :spd
+      t.integer :stat_pts
+
+      t.timestamps
+    end
+  end
+end
