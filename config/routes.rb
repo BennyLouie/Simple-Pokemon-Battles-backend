0403adb5a6b3774c0b460bcb781c88e859030c7e
BennyLouie <bennylouie1412@gmail.com>
2019-11-27 18:20:59 -0500

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :captures
   resources :users
   resources :pokemons
+  resources :login, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
