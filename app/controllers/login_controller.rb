6211de1e054f4d9392dcfa721cc3144943ffac39
BennyLouie <bennylouie1412@gmail.com>
2019-12-04 17:23:10 -0500

    changed error rendering

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,8 +4,10 @@ class LoginController < ApplicationController
         user = User.find_by("lower(username) = ?", params[:username].downcase)
         if user && user.authenticate(params[:password])
             render json: { token: create_token(user.id), user_id: user.id }
-        else
-            render json: { errors: [ "Sorry, we couldn't find that user in our database." ] }, status: :unprocessable_entity
+        elsif user && !user.authenticate(params[:password])
+            render json: { errors: "Sorry wrong password" }, status: :unprocessable_entity
+        elsif !user
+            render json: { errors: "Sorry, we couldn't find that user in our database." }, status: :unprocessable_entity
         end
     end
 end
