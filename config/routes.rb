3a81259d79a8cfa0c45bde03befbaf7c2a6ed61f
BennyLouie <bennylouie1412@gmail.com>
2019-12-05 11:12:36 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :captures, except: [:new, :update, :edit]
-  resources :users, except: [:new, :edit, :destroy]
+  resources :users, except: [:new, :edit]
   resources :pokemons, except: [:new, :edit]
   resources :login, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
