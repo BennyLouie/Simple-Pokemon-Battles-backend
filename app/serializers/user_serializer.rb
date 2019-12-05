1e5df350cd7f66d50fcef6a0185652bd51eca1f6
BennyLouie <bennylouie1412@gmail.com>
2019-12-05 14:35:24 -0500

    added first_name to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :username, :password_digest, :wins, :losses
+  attributes :id, :full_name, :first_name, :username, :password_digest, :wins, :losses
   has_many :pokemons
 end
